719d84ef0f010df1da40c8a49584a23d30d85d9a
Bongani Nyongwane <nyongwanebongani@gmail.com>
2025-02-17 10:54:48 +0200

    Initial commit

---
+++ recipe-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header"
+import MainS from "./MainS"
+
+export default function App(){
+  return(
+    <>
+    <Header />
+    <MainS />
+    </>
+    
+  )
+}
---
+++ recipe-react-app/src/MainS.jsx
@@ -0,0 +1,14 @@
+export default function MainS() {
+  return (
+    <main>
+      <form className="add-ingredient-form">
+        <input
+          type="text"
+          placeholder="e.g orange"
+          aria-label="Add ingredients "
+        />
+        <button>Add ingrdeint</button>
+      </form>
+    </main>
+  );
+}
